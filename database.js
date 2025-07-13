01868d2950d5b83a75d4662b3d2c6dc6d0eefde2
Mario Salazar <mario-0709@hotmail.com>
2025-07-13 18:38:51 -0500

    Primer commit - Consulta - Gastos

--- database.js
+++ database.js
@@ -4,7 +4,10 @@ const path = require('path');
 const Gasto = require('./data/gastos'); // Asegúrate de que el modelo esté bien definido
 
 // Conexión a MongoDB
-mongoose.connect('mongodb://localhost:27017/consultasDT')
+
+//mongoose.connect('mongodb://localhost:27017/consultasDT')
+
+const URI = mongodb://atlas-sql-686e8649c0f71550b09879b0-pnvdmh.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin
 
 .then(async () => {
   console.log('✅ Conexión a MongoDB establecida');
