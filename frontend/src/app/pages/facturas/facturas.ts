5764944759745512661843685dd9dd7ef3e97b8c
mario-alexander-salazar <mario-0709@hotmail.com>
2025-07-25 22:25:53 -0500

    Corrijo estructura del frontend

---
+++ frontend/src/app/pages/facturas/facturas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-facturas',
+  imports: [],
+  templateUrl: './facturas.html',
+  styleUrl: './facturas.css'
+})
+export class Facturas {
+
+}
